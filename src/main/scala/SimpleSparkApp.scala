42e27ee32be482c245977328f30593949ea99e92
calvincodes <arpitjain1821@gmail.com>
2019-09-21 11:39:34 -0500

    Taking input and output file paths as cmd line args

--- src/main/scala/SimpleSparkApp.scala
+++ src/main/scala/SimpleSparkApp.scala
@@ -4,12 +4,12 @@ import org.apache.spark.SparkContext
 object SimpleSparkApp {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Simple App")
+
+    val conf = new SparkConf().setAppName("SimpleSparkApp")
     val sc = new SparkContext(conf)
 
-    //TODO: Both of these will be passed by the caller
-    val inputLocation = "hdfs://128.104.223.172:9000/users/ajain/hdfs/input/part2_data.csv"
-    val outputLocation = "hdfs://128.104.223.172:9000/users/ajain/hdfs/output/"
+    val inputLocation = args(0) // For testing use "hdfs://128.104.223.172:9000/users/ajain/hdfs/input/part2_data.csv"
+    val outputLocation = args(1) // For testing use "hdfs://128.104.223.172:9000/users/ajain/hdfs/output/"
 
     val rawData = sc.textFile(inputLocation)
 
