6662849bed5f1f6f1334379f9ae4a4e6d9203295
calvincodes <arpitjain1821@gmail.com>
2019-09-21 11:20:30 -0500

    Working code for part 2 with todo of getting file paths from user

--- src/main/scala/SimpleSparkApp.scala
+++ src/main/scala/SimpleSparkApp.scala
@@ -7,9 +7,20 @@ object SimpleSparkApp {
     val conf = new SparkConf().setAppName("Simple App")
     val sc = new SparkContext(conf)
 
-    val fileLocation = "hdfs://128.104.223.172:9000/users/ajain/hdfs/part2_data.csv"
-    val rawFile = sc.textFile(fileLocation)
-    rawFile.map(s => s.length).reduce((a, b) => a + b)
+    //TODO: Both of these will be passed by the caller
+    val inputLocation = "hdfs://128.104.223.172:9000/users/ajain/hdfs/input/part2_data.csv"
+    val outputLocation = "hdfs://128.104.223.172:9000/users/ajain/hdfs/output/"
+
+    val rawData = sc.textFile(inputLocation)
+
+    val ccTimestampToLine
+      = rawData.map(line => (line.split(",")(3).hashCode + line.split(",")(14), line))
+
+    val sortedRdd = ccTimestampToLine.sortByKey()
+
+    val resultRdd = sortedRdd.map(line => line._2)
+
+    resultRdd.saveAsTextFile(outputLocation)
   }
 
 }
