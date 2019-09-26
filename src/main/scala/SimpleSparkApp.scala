8e564c2cd69d71853be53454cbf767ad1a5247b8
calvincodes <arpitjain1821@gmail.com>
2019-09-25 19:23:00 -0500

    Added comments

--- src/main/scala/SimpleSparkApp.scala
+++ src/main/scala/SimpleSparkApp.scala
@@ -6,22 +6,31 @@ object SimpleSparkApp {
 
   def main(args: Array[String]): Unit = {
 
+    // Get SparkContext
     val conf = new SparkConf().setAppName("SimpleSparkApp")
     val sc = new SparkContext(conf)
 
-    val inputLocation = args(0) // For testing use "hdfs://10.10.1.1:9000/input_2/part2_data.csv"
-    val outputLocation = args(1) // For testing use "hdfs://10.10.1.1:9000/output_2/"
+    // Fetch input and output file locations from input args
+    val inputLocation = args(0)
+    val outputLocation = args(1)
 
-    val rawData = sc.textFile(inputLocation)
+    // Read the data from input file into RDD
+    val rawDataRdd = sc.textFile(inputLocation)
 
-    val filteredRdd = rawData.filter(!_.startsWith("battery_level")).map(line => line.split(","))
-    val sortedRdd = filteredRdd.sortBy(r => (r(2), r(14)))
+    // Step 1: Filter rows keeping only the rows which have actual data in them
+    // Step 2: Split the content of each row using deliminiter ","
+    // Store the result in filterArrayRdd -> each row is an array of strings
+    val filterArrayRdd = rawDataRdd.filter(!_.startsWith("battery_level")).map(line => line.split(","))
 
+    // Sort filterArrayRdd on 3rd and last column. Store result in sortedRdd
+    val sortedRdd = filterArrayRdd.sortBy(r => (r(2), r(14)))
+
+    // Convert each row of sortedRdd from Array[String] to String
     val resultRdd = sortedRdd.map(line => line.mkString(","))
 
+    // Save resultRdd as a text file at user provided outputLocation
     resultRdd.saveAsTextFile(outputLocation)
 
     sc.stop()
   }
-
 }
